edabf6c7d5852acbe87f2039632593ca3ba3d299
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-12-15 16:32:33 +0900

    fix s3 origin request policy

--- terraform/projects/some_service_a/staging/cdn/datasource.tf
+++ terraform/projects/some_service_a/staging/cdn/datasource.tf
@@ -7,3 +7,14 @@ data "terraform_remote_state" "origin_access_identity" {
     region = local.backend_config.region
   }
 }
+
+data "terraform_remote_state" "frontend" {
+  backend = "s3"
+
+  config = {
+    bucket = local.backend_config.bucket
+    key = "${local.backend_config.prefix}/frontend"
+    region = local.backend_config.region
+  }
+
+}
--- terraform/projects/some_service_a/staging/cdn/main.tf
+++ terraform/projects/some_service_a/staging/cdn/main.tf
@@ -56,24 +56,52 @@ module "distribution" {
           value = local.backend_config.cloudfront_elb_pre_shared_key
         }
       ]
+    },
+    fronted = {
+      origin_id = "frontend"
+      domain_name = data.terraform_remote_state.frontend.outputs.s3_bucket_bucket_regional_domain_name
+
+      s3_origin_config = {
+        cloudfront_access_identity_path = data.terraform_remote_state.origin_access_identity.outputs.cloudfront_access_identity_path
+      }
     }
   }
 
   default_cache_behavior = {
-    target_origin_id = "elb_backend"
+    target_origin_id = "frontend"
     viewer_protocol_policy = "redirect-to-https"
 
-    allowed_method = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    allowed_method = ["HEAD", "GET"]
     cached_methods = ["HEAD", "GET"]
     compress = true
 
     use_forwarded_values = false
     // This is id for SecurityHeadersPolicy copied from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-response-headers-policies.html
     response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
-    origin_request_policy_id = module.cloudfront_policy.origin_request_policy_id
+    // This is id for CORS-S3Origin Policy copied from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-origin-request-policies.html
+    origin_request_policy_id = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"
     // This is id for CacheDiabledPolicy copied from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
     cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
   }
+
+  ordered_cache_behavior = [
+    {
+      path_pattern = "/api/*"
+      target_origin_id = "elb_backend"
+      viewer_protocol_policy = "redirect-to-https"
+
+      allowed_method = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+      cached_methods = ["HEAD", "GET"]
+      compress = true
+
+      use_forwarded_values = false
+      // This is id for SecurityHeadersPolicy copied from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-response-headers-policies.html
+      response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
+      origin_request_policy_id = module.cloudfront_policy.origin_request_policy_id
+      // This is id for CacheDiabledPolicy copied from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
+      cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    }
+  ]
 
   viewer_certificate = {
     acm_certificate_arn = data.aws_acm_certificate.issued.arn
