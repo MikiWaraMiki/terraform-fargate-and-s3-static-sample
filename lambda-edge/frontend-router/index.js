c74ef480469a2353fde0a9152f40621afa5fbe66
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-12-19 16:27:17 +0900

    fix router lambda edge

--- lambda-edge/frontend-router/index.js
+++ lambda-edge/frontend-router/index.js
@@ -1,35 +1,54 @@
-'use strict'
+"use strict";
 
-const routes = require('./routes')
+const routes = require("./routes");
 
+const isEndSlash = (route) => {
+  const filename = route.split("/").pop();
+
+  return !filename;
+};
 const notHtmlPath = (route) => {
-  const routeStrArr = route.split('.')
-  routeStrArr.shift()
-  const extension = routeStrArr.pop()
-  return !!extension && !extension.match(/html?$/)
-}
+  const routeStrArr = route.split(".");
+  routeStrArr.shift();
+  const extension = routeStrArr.pop();
+  return !!extension && !extension.match(/html?$/);
+};
 const dynamicRouting = (url) => {
-  if(url === '/') return '/index.html'
-  for(let route of routes) {
-    if(url === route) return route
-    const rulePlaceHolder = /\[[^/].*\]/g
-    if(!route.match(rulePlaceHolder)) continue;
-    const matchCase = new RegExp(route.replace(rulePlaceHolder,'[^/].*'))
-    if(url.match(matchCase)){
-      return `${url.replace(matchCase, route)}.html`
+  if (url === "/") return "/index.html";
+  for (let route of routes) {
+    if (url === route) return `${route}.html`;
+    const rulePlaceHolder = /\[[^/].*\]/g;
+    if (!route.match(rulePlaceHolder)) continue;
+    const matchCase = new RegExp(route.replace(rulePlaceHolder, "[^/].*"));
+    if (url.match(matchCase)) {
+      return `${url.replace(matchCase, route)}.html`;
     }
   }
-  return '/404.html'
-}
+  return "/404.html";
+};
 exports.handler = (event, _context, callback) => {
   const request = event.Records[0].cf.request;
-  if(notHtmlPath(request.uri)) {
+  if (isEndSlash(request.uri)) {
+    const response = {
+      status: "302",
+      statusDescription: "Found",
+      headers: {
+        location: [
+          {
+            key: "Location",
+            value: request.uri.replace(/\/*$/, "") || "/",
+          },
+        ],
+      },
+    };
+    callback(null, response);
+    return;
+  }
+  if (notHtmlPath(request.uri)) {
     callback(null, request);
     return;
   }
-  request.uri = dynamicRouting(
-    request.uri.replace('.html', '')
-  );
+  request.uri = dynamicRouting(request.uri.replace(".html", ""));
   callback(null, request);
   return;
-}
+};
--- lambda-edge/frontend-router/routes.js
+++ lambda-edge/frontend-router/routes.js
@@ -1,7 +1,11 @@
-'use strict'
+"use strict";
 
 // WARNING: 新しいDynamic Routeを追加する度に更新の必要がある
 // NOTE: Static RouteはDynamic Routeより先に検証する必要がある
 module.exports = [
-  '/index',
-]
+  "/index",
+  "/page1",
+  // NOTE: ネストしたルーティングは先に書く必要がある
+  "/dynamics/[id]/nested",
+  "/dynamics/[id]",
+];
